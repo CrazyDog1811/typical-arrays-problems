2eb3a480f2893e50422c769d784b2f42ed332235
CrazyDog1811 <evgeshatertyshnyi@mail.ru>
2021-11-21 19:36:40 +0300

    feat: create functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+  if(array.length === 1) return array[0];
+ return array.sort((a, b) => a - b).shift() ;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array  || array.length === 0) return 0;
+  if(array.length === 1) return array[0];
+  return array.sort((a, b) => a - b).pop() ;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array  || array.length === 0) return 0;
+  if(array.length === 1) return array[0];
+  return array.reduce((a, b) => a + b) / array.length;
 }
